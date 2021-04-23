f08787b80ac84f2181c60842bd7d00cf5f927d50
Chris Dunphy <chris@megaparsec.ca>
2021-04-22 21:55:22 -0600

    Add Roflania settings for VSCode and Sublime

---
+++ VSCode/Roflania/install-exts.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+code --install-extension bungcip.better-toml
+code --install-extension haskell.haskell
+code --install-extension justusadam.language-haskell
+code --install-extension lampepfl.dotty
+code --install-extension matklad.rust-analyzer
+code --install-extension monokai.theme-monokai-pro-vscode
+code --install-extension ms-python.python
+code --install-extension redhat.java
+code --install-extension redhat.vscode-xml
+code --install-extension redhat.vscode-yaml
+code --install-extension VisualStudioExptTeam.vscodeintellicode
+code --install-extension vscjava.vscode-java-debug
+code --install-extension vscjava.vscode-java-dependency
+code --install-extension vscjava.vscode-java-pack
+code --install-extension vscjava.vscode-java-test
