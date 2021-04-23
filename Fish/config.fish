647625e2b8a2d52dedfbb244fe5262d3cd2a8784
Chris Dunphy <chris@megaparsec.ca>
2021-04-22 18:03:43 -0600

    Make fish_ssh_agent macos

--- Fish/config.fish
+++ Fish/config.fish
@@ -11,6 +11,9 @@ alias ll="exa -lah"
 alias wx="curl wttr.in/Calgary"
 alias moon="curl wttr.in/moon"
 
+# dust is a Rust replacement for du
+alias du="dust"
+
 # Base16 Shell
 if status --is-interactive
     set BASE16_SHELL "$HOME/.config/base16-shell/"
@@ -18,10 +21,12 @@ if status --is-interactive
 end
 
 # fish-ssh-agent
-if test -z "$SSH_ENV"
-    set -xg SSH_ENV $HOME/.ssh/environment
+switch (uname)
+case Darwin
+    if test -z "$SSH_ENV"
+        set -xg SSH_ENV $HOME/.ssh/environment
+    end
+    if not __ssh_agent_is_started
+        __ssh_agent_start
+    end
 end
-if not __ssh_agent_is_started
-    __ssh_agent_start
-end
-
