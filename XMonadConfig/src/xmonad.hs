c104d4dc311d622abfef57ef1dcf2769e2985c2e
Chris Dunphy <chris@megaparsec.ca>
2021-05-12 20:36:04 -0600

    Fixes for XMonad on my PopOS install

--- XMonadConfig/src/xmonad.hs
+++ XMonadConfig/src/xmonad.hs
@@ -1,19 +1,19 @@
-import qualified Data.Map                     as M
-import           Data.Monoid
-import           System.Exit
-import           XMonad
-import           XMonad.Hooks.DynamicLog
-import           XMonad.Hooks.ManageDocks
-import           XMonad.Hooks.SetWMName
-import           XMonad.Layout.Grid
-import           XMonad.Layout.LayoutModifier
-import           XMonad.Layout.Renamed        (Rename (Replace), renamed)
-import           XMonad.Layout.Spacing
-import qualified XMonad.StackSet              as W
-import           XMonad.Util.Run
-import           XMonad.Util.SpawnOnce
+import qualified Data.Map as M
+import Data.Monoid
+import System.Exit
+import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.SetWMName
+import XMonad.Layout.Grid
+import XMonad.Layout.LayoutModifier
+import XMonad.Layout.Renamed (Rename (Replace), renamed)
+import XMonad.Layout.Spacing
+import qualified XMonad.StackSet as W
+import XMonad.Util.Run
+import XMonad.Util.SpawnOnce
 
-myTerminal      = "alacritty"
+myTerminal = "alacritty"
 
 myFocusFollowsMouse :: Bool
 myFocusFollowsMouse = True
@@ -21,116 +21,99 @@ myFocusFollowsMouse = True
 myClickJustFocuses :: Bool
 myClickJustFocuses = False
 
-myBorderWidth   = 2
+myBorderWidth = 2
 
-myModMask       = mod4Mask
+myModMask = mod4Mask
 
 myWorkspaces = ["alpha", "beta", "gamma", "delta"]
 
-myNormalBorderColor  = "#aaaaaa"
+myNormalBorderColor = "#aaaaaa"
+
 myFocusedBorderColor = "#eeeeee"
 
-myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
-
+myKeys conf@(XConfig {XMonad.modMask = modm}) =
+  M.fromList $
     -- launch a terminal
-    [ ((modm .|. shiftMask, xK_Return), spawn $ XMonad.terminal conf)
-
-    -- launch dmenu
-    , ((modm,               xK_p     ), spawn "dmenu_run -p 'SuperJeb' -fn 'Noto Sans Mono:pixelsize=18' -nb '#224466' -sf '#224466' -sb '#d0d0d0' -nf '#d0d0d0'")
-
-    -- lock screen
-    , ((modm,               xK_z     ), spawn "loginctl lock-session")
-
-    -- put computer to sleep
-    , ((modm .|. shiftMask, xK_z     ), spawn "systemctl suspend")
-
-    -- close focused window
-    , ((modm .|. shiftMask, xK_c     ), kill)
-
-     -- Rotate through the available layout algorithms
-    , ((modm,               xK_space ), sendMessage NextLayout)
-
-    --  Reset the layouts on the current workspace to default
-    , ((modm .|. shiftMask, xK_space ), setLayout $ XMonad.layoutHook conf)
-
-    -- Resize viewed windows to the correct size
-    , ((modm,               xK_n     ), refresh)
-
-    -- Move focus to the next window
-    , ((modm,               xK_Tab   ), windows W.focusDown)
-
-    -- Move focus to the next window
-    , ((modm,               xK_j     ), windows W.focusDown)
-
-    -- Move focus to the previous window
-    , ((modm,               xK_k     ), windows W.focusUp  )
-
-    -- Move focus to the master window
-    , ((modm,               xK_m     ), windows W.focusMaster  )
-
-    -- Swap the focused window and the master window
-    , ((modm,               xK_Return), windows W.swapMaster)
-
-    -- Swap the focused window with the next window
-    , ((modm .|. shiftMask, xK_j     ), windows W.swapDown  )
-
-    -- Swap the focused window with the previous window
-    , ((modm .|. shiftMask, xK_k     ), windows W.swapUp    )
-
-    -- Shrink the master area
-    , ((modm,               xK_h     ), sendMessage Shrink)
-
-    -- Expand the master area
-    , ((modm,               xK_l     ), sendMessage Expand)
-
-    -- Push window back into tiling
-    , ((modm,               xK_t     ), withFocused $ windows . W.sink)
-
-    -- Increment the number of windows in the master area
-    , ((modm              , xK_comma ), sendMessage (IncMasterN 1))
-
-    -- Deincrement the number of windows in the master area
-    , ((modm              , xK_period), sendMessage (IncMasterN (-1)))
-
-    -- Toggle the status bar gap
-    , ((modm              , xK_b     ), sendMessage ToggleStruts)
-
-    -- Quit xmonad
-    , ((modm .|. shiftMask, xK_q     ), io (exitWith ExitSuccess))
-
-    -- Restart xmonad
-    , ((modm              , xK_q     ), spawn "xmonad --recompile; xmonad --restart")
-
-    -- Run xmessage with a summary of the default keybindings (useful for beginners)
-    , ((modm .|. shiftMask, xK_slash ), spawn ("echo \"" ++ help ++ "\" | xmessage -file -"))
+    [ ((modm .|. shiftMask, xK_Return), spawn $ XMonad.terminal conf),
+      -- launch dmenu
+      ((modm, xK_p), spawn "dmenu_run -p 'SuperJeb' -fn 'Noto Sans Mono:pixelsize=18' -nb '#224466' -sf '#224466' -sb '#d0d0d0' -nf '#d0d0d0'"),
+      -- lock screen
+      ((modm, xK_z), spawn "loginctl lock-session"),
+      -- put computer to sleep
+      ((modm .|. shiftMask, xK_z), spawn "systemctl suspend"),
+      -- close focused window
+      ((modm .|. shiftMask, xK_c), kill),
+      -- Rotate through the available layout algorithms
+      ((modm, xK_space), sendMessage NextLayout),
+      --  Reset the layouts on the current workspace to default
+      ((modm .|. shiftMask, xK_space), setLayout $ XMonad.layoutHook conf),
+      -- Resize viewed windows to the correct size
+      ((modm, xK_n), refresh),
+      -- Move focus to the next window
+      ((modm, xK_Tab), windows W.focusDown),
+      -- Move focus to the next window
+      ((modm, xK_j), windows W.focusDown),
+      -- Move focus to the previous window
+      ((modm, xK_k), windows W.focusUp),
+      -- Move focus to the master window
+      ((modm, xK_m), windows W.focusMaster),
+      -- Swap the focused window and the master window
+      ((modm, xK_Return), windows W.swapMaster),
+      -- Swap the focused window with the next window
+      ((modm .|. shiftMask, xK_j), windows W.swapDown),
+      -- Swap the focused window with the previous window
+      ((modm .|. shiftMask, xK_k), windows W.swapUp),
+      -- Shrink the master area
+      ((modm, xK_h), sendMessage Shrink),
+      -- Expand the master area
+      ((modm, xK_l), sendMessage Expand),
+      -- Push window back into tiling
+      ((modm, xK_t), withFocused $ windows . W.sink),
+      -- Increment the number of windows in the master area
+      ((modm, xK_comma), sendMessage (IncMasterN 1)),
+      -- Deincrement the number of windows in the master area
+      ((modm, xK_period), sendMessage (IncMasterN (-1))),
+      -- Toggle the status bar gap
+      ((modm, xK_b), sendMessage ToggleStruts),
+      -- Quit xmonad
+      ((modm .|. shiftMask, xK_q), io (exitWith ExitSuccess)),
+      -- Restart xmonad
+      ((modm, xK_q), spawn "xmonad --recompile; xmonad --restart"),
+      -- Run xmessage with a summary of the default keybindings (useful for beginners)
+      ((modm .|. shiftMask, xK_slash), spawn ("echo \"" ++ help ++ "\" | xmessage -file -"))
     ]
-    ++
-
-    --
-    -- mod-[1..9], Switch to workspace N
-    -- mod-shift-[1..9], Move client to workspace N
-    --
-    [((m .|. modm, k), windows $ f i)
-        | (i, k) <- zip (XMonad.workspaces conf) [xK_1 .. xK_9]
-        , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]]
+      ++
+      --
+      -- mod-[1..9], Switch to workspace N
+      -- mod-shift-[1..9], Move client to workspace N
+      --
+      [ ((m .|. modm, k), windows $ f i)
+        | (i, k) <- zip (XMonad.workspaces conf) [xK_1 .. xK_9],
+          (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]
+      ]
 
 ------------------------------------------------------------------------
 -- Mouse bindings: default actions bound to mouse events
 --
-myMouseBindings (XConfig {XMonad.modMask = modm}) = M.fromList $
-
+myMouseBindings (XConfig {XMonad.modMask = modm}) =
+  M.fromList $
     -- mod-button1, Set the window to floating mode and move by dragging
-    [ ((modm, button1), (\w -> focus w >> mouseMoveWindow w
-                                       >> windows W.shiftMaster))
-
-    -- mod-button2, Raise the window to the top of the stack
-    , ((modm, button2), (\w -> focus w >> windows W.shiftMaster))
-
-    -- mod-button3, Set the window to floating mode and resize by dragging
-    , ((modm, button3), (\w -> focus w >> mouseResizeWindow w
-                                       >> windows W.shiftMaster))
-
-    -- you may also bind events to the mouse scroll wheel (button4 and button5)
+    [ ( (modm, button1),
+        ( \w ->
+            focus w >> mouseMoveWindow w
+              >> windows W.shiftMaster
+        )
+      ),
+      -- mod-button2, Raise the window to the top of the stack
+      ((modm, button2), (\w -> focus w >> windows W.shiftMaster)),
+      -- mod-button3, Set the window to floating mode and resize by dragging
+      ( (modm, button3),
+        ( \w ->
+            focus w >> mouseResizeWindow w
+              >> windows W.shiftMaster
+        )
+      )
+      -- you may also bind events to the mouse scroll wheel (button4 and button5)
     ]
 
 ------------------------------------------------------------------------
@@ -145,26 +128,28 @@ mySpacing i = spacingRaw False (Border i i i i) True (Border i i i i) True
 -- which denotes layout choice.
 myLayout = avoidStruts $ tiled ||| grid ||| mirror ||| full
   where
-     tiled   = renamed [Replace "Tiled"] $ mySpacing 5 $ Tall nmaster delta ratio
-     grid    = renamed [Replace "Grid"] $ mySpacing 5 $ Grid
-     mirror  = renamed [Replace "Vertical Tiled"] $ Mirror tiled
-     full    = renamed [Replace "Full"] $ Full
+    tiled = renamed [Replace "Tiled"] $ mySpacing 5 $ Tall nmaster delta ratio
+    grid = renamed [Replace "Grid"] $ mySpacing 5 $ Grid
+    mirror = renamed [Replace "Vertical Tiled"] $ Mirror tiled
+    full = renamed [Replace "Full"] $ Full
 
-     -- The default number of windows in the master pane
-     nmaster = 1
-     -- Default proportion of screen occupied by master pane
-     ratio   = 1/2
-     -- Percent of screen to increment by when resizing panes
-     delta   = 3/100
+    -- The default number of windows in the master pane
+    nmaster = 1
+    -- Default proportion of screen occupied by master pane
+    ratio = 1 / 2
+    -- Percent of screen to increment by when resizing panes
+    delta = 3 / 100
 
 ------------------------------------------------------------------------
 -- Window rules:
 
-myManageHook = composeAll
-    [ className =? "MPlayer"        --> doFloat
-    , className =? "Gimp"           --> doFloat
-    , resource  =? "desktop_window" --> doIgnore
-    , resource  =? "kdesktop"       --> doIgnore ]
+myManageHook =
+  composeAll
+    [ className =? "MPlayer" --> doFloat,
+      className =? "Gimp" --> doFloat,
+      resource =? "desktop_window" --> doIgnore,
+      resource =? "kdesktop" --> doIgnore
+    ]
 
 ------------------------------------------------------------------------
 -- Event handling
@@ -176,89 +161,95 @@ myStartupHook = do
   setWMName "LG3D"
   spawnOnce "picom &"
   spawnOnce "nitrogen --restore &"
-  spawnOnce "xss-lock /usr/bin/slock"
+  spawnOnce "xss-lock /usr/local/bin/slock"
 
 -- Run xmonad with the settings you specify.
 main = do
   xmproc <- spawnPipe "xmobar /home/cdunphy/.config/xmobar/xmobar.cfg"
-  xmonad $ docks defaults {
-    logHook = dynamicLogWithPP $ xmobarPP {
-        ppOutput = \x -> hPutStrLn xmproc x
-        , ppCurrent = xmobarColor "#c3e88d" "" . wrap "[" "]" -- Current workspace in xmobar
-        , ppVisible = xmobarColor "#999999" ""                -- Visible but not current workspace
-        , ppHidden = xmobarColor "#77a88d" "" . wrap "*" ""   -- Hidden workspaces in xmobar
-        , ppHiddenNoWindows = xmobarColor "#77a88d" ""        -- Hidden workspaces (no windows)
-        , ppTitle = xmobarColor "#d0d0d0" "" . shorten 60     -- Title of active window in xmobar
-        , ppSep =  "<fc=#666666> | </fc>"                     -- Separators in xmobar
-    }
-  }
+  xmonad $
+    docks
+      defaults
+        { logHook =
+            dynamicLogWithPP $
+              xmobarPP
+                { ppOutput = \x -> hPutStrLn xmproc x,
+                  ppCurrent = xmobarColor "#c3e88d" "" . wrap "[" "]", -- Current workspace in xmobar
+                  ppVisible = xmobarColor "#999999" "", -- Visible but not current workspace
+                  ppHidden = xmobarColor "#77a88d" "" . wrap "*" "", -- Hidden workspaces in xmobar
+                  ppHiddenNoWindows = xmobarColor "#77a88d" "", -- Hidden workspaces (no windows)
+                  ppTitle = xmobarColor "#d0d0d0" "" . shorten 60, -- Title of active window in xmobar
+                  ppSep = "<fc=#666666> | </fc>" -- Separators in xmobar
+                }
+        }
 
-defaults = def {
-      -- simple stuff
-        terminal           = myTerminal,
-        focusFollowsMouse  = myFocusFollowsMouse,
-        clickJustFocuses   = myClickJustFocuses,
-        borderWidth        = myBorderWidth,
-        modMask            = myModMask,
-        workspaces         = myWorkspaces,
-        normalBorderColor  = myNormalBorderColor,
-        focusedBorderColor = myFocusedBorderColor,
-
+defaults =
+  def
+    { -- simple stuff
+      terminal = myTerminal,
+      focusFollowsMouse = myFocusFollowsMouse,
+      clickJustFocuses = myClickJustFocuses,
+      borderWidth = myBorderWidth,
+      modMask = myModMask,
+      workspaces = myWorkspaces,
+      normalBorderColor = myNormalBorderColor,
+      focusedBorderColor = myFocusedBorderColor,
       -- key bindings
-        keys               = myKeys,
-        mouseBindings      = myMouseBindings,
-
+      keys = myKeys,
+      mouseBindings = myMouseBindings,
       -- hooks, layouts
-        layoutHook         = myLayout,
-        manageHook         = myManageHook,
-        handleEventHook    = myEventHook,
-        startupHook        = myStartupHook
+      layoutHook = myLayout,
+      manageHook = myManageHook,
+      handleEventHook = myEventHook,
+      startupHook = myStartupHook
     }
 
 -- | Finally, a copy of the default bindings in simple textual tabular format.
 help :: String
-help = unlines ["The default modifier key is 'alt'. Default keybindings:",
-    "",
-    "-- launching and killing programs",
-    "mod-Shift-Enter  Launch xterminal",
-    "mod-p            Launch dmenu",
-    "mod-Shift-c      Close/kill the focused window",
-    "mod-Space        Rotate through the available layout algorithms",
-    "mod-Shift-Space  Reset the layouts on the current workSpace to default",
-    "mod-n            Resize/refresh viewed windows to the correct size",
-    "",
-    "-- move focus up or down the window stack",
-    "mod-Tab        Move focus to the next window",
-    "mod-Shift-Tab  Move focus to the previous window",
-    "mod-j          Move focus to the next window",
-    "mod-k          Move focus to the previous window",
-    "mod-m          Move focus to the master window",
-    "",
-    "-- modifying the window order",
-    "mod-Return   Swap the focused window and the master window",
-    "mod-Shift-j  Swap the focused window with the next window",
-    "mod-Shift-k  Swap the focused window with the previous window",
-    "",
-    "-- resizing the master/slave ratio",
-    "mod-h  Shrink the master area",
-    "mod-l  Expand the master area",
-    "",
-    "-- floating layer support",
-    "mod-t  Push window back into tiling; unfloat and re-tile it",
-    "",
-    "-- increase or decrease number of windows in the master area",
-    "mod-comma  (mod-,)   Increment the number of windows in the master area",
-    "mod-period (mod-.)   Deincrement the number of windows in the master area",
-    "",
-    "-- quit, or restart",
-    "mod-Shift-q  Quit xmonad",
-    "mod-q        Restart xmonad",
-    "mod-[1..9]   Switch to workSpace N",
-    "",
-    "-- Workspaces & screens",
-    "mod-Shift-[1..9]   Move client to workspace N",
-    "",
-    "-- Mouse bindings: default actions bound to mouse events",
-    "mod-button1  Set the window to floating mode and move by dragging",
-    "mod-button2  Raise the window to the top of the stack",
-    "mod-button3  Set the window to floating mode and resize by dragging"]
+help =
+  unlines
+    [ "The default modifier key is 'alt'. Default keybindings:",
+      "",
+      "-- launching and killing programs",
+      "mod-Shift-Enter  Launch xterminal",
+      "mod-p            Launch dmenu",
+      "mod-Shift-c      Close/kill the focused window",
+      "mod-Space        Rotate through the available layout algorithms",
+      "mod-Shift-Space  Reset the layouts on the current workSpace to default",
+      "mod-n            Resize/refresh viewed windows to the correct size",
+      "",
+      "-- move focus up or down the window stack",
+      "mod-Tab        Move focus to the next window",
+      "mod-Shift-Tab  Move focus to the previous window",
+      "mod-j          Move focus to the next window",
+      "mod-k          Move focus to the previous window",
+      "mod-m          Move focus to the master window",
+      "",
+      "-- modifying the window order",
+      "mod-Return   Swap the focused window and the master window",
+      "mod-Shift-j  Swap the focused window with the next window",
+      "mod-Shift-k  Swap the focused window with the previous window",
+      "",
+      "-- resizing the master/slave ratio",
+      "mod-h  Shrink the master area",
+      "mod-l  Expand the master area",
+      "",
+      "-- floating layer support",
+      "mod-t  Push window back into tiling; unfloat and re-tile it",
+      "",
+      "-- increase or decrease number of windows in the master area",
+      "mod-comma  (mod-,)   Increment the number of windows in the master area",
+      "mod-period (mod-.)   Deincrement the number of windows in the master area",
+      "",
+      "-- quit, or restart",
+      "mod-Shift-q  Quit xmonad",
+      "mod-q        Restart xmonad",
+      "mod-[1..9]   Switch to workSpace N",
+      "",
+      "-- Workspaces & screens",
+      "mod-Shift-[1..9]   Move client to workspace N",
+      "",
+      "-- Mouse bindings: default actions bound to mouse events",
+      "mod-button1  Set the window to floating mode and move by dragging",
+      "mod-button2  Raise the window to the top of the stack",
+      "mod-button3  Set the window to floating mode and resize by dragging"
+    ]
