b0d004261b2a665f6b5fe318ad4c52f1c4533353
Chris Dunphy <chris@megaparsec.ca>
2021-05-27 15:10:06 -0600

    Fix fish config for pyenv 2.0

--- Fish/Roflania/config.fish
+++ Fish/Roflania/config.fish
@@ -29,12 +29,6 @@ alias du="dust"
 alias ls="exa"
 alias ll="exa -lah"
 
-# Add pyenv executable to PATH by running
-# the following interactively:
-set -Ux PYENV_ROOT $HOME/.pyenv
-set -U fish_user_paths $PYENV_ROOT/bin $fish_user_paths
-set -U fish_user_paths $HOME/.local/bin $fish_user_paths
-
 # Load pyenv automatically by appending
 # the following to ~/.config/fish/config.fish:
 status is-login; and pyenv init --path | source
