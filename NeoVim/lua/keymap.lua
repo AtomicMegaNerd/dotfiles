aba7b6f6803ee8fc12c4f99a773a24c4cb9273f1
Chris Dunphy <chris.dunphy@fglsports.com>
2021-11-17 07:47:08 -0700

    Local keymap changes

--- NeoVim/lua/keymap.lua
+++ NeoVim/lua/keymap.lua
@@ -15,7 +15,7 @@ vim.api.nvim_set_keymap('n', '<right>', '<cmd>bn<cr>', opts)
 -- Telescope keybindings
 vim.api.nvim_set_keymap('n', '<leader>ff', [[<cmd>Telescope find_files<CR>]], opts)
 vim.api.nvim_set_keymap('n', '<leader>fg', [[<cmd>Telescope live_grep<CR>]], opts)
-vim.api.nvim_set_keymap('n', '<leader>ft', [[<cmd>Telescope git_files<CR>]], opts)
+vim.api.nvim_set_keymap('n', '<leader>ft', [[<cmd>Telescope treesitter<CR>]], opts)
 vim.api.nvim_set_keymap('n', '<leader>fb', [[<cmd>Telescope buffers<CR>]], opts)
 vim.api.nvim_set_keymap('n', '<leader>fh', [[<cmd>Telescope help_tags<CR>]], opts)
 
