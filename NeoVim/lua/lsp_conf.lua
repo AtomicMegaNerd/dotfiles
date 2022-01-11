c4764566b878b7d966cf3507c931a41603501104
Chris Dunphy <rcd@Discovery.local>
2022-01-10 20:39:57 -0700

    Discovery is the new host that replaces Roflania
    Lots of updates for Discovery.

--- NeoVim/lua/lsp_conf.lua
+++ NeoVim/lua/lsp_conf.lua
@@ -44,7 +44,7 @@ local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
 
 -- Enable the following language servers
-local servers = { "hls", "gopls", "pyright", "yamlls" }
+local servers = { "hls", "gopls", "pyright", "yamlls", "ansiblels" }
 for _, lsp in ipairs(servers) do
 	nvim_lsp[lsp].setup({
 		on_attach = on_attach,
