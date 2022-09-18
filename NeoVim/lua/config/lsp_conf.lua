e34c242823c7c6e96c2f3a328a5dcab9f3ecdfc0
Chris Dunphy <chris@megaparsec.ca>
2022-09-18 08:27:31 -0600

    More minor cleanup

--- NeoVim/lua/config/lsp_conf.lua
+++ NeoVim/lua/config/lsp_conf.lua
@@ -17,7 +17,7 @@ local on_attach = function(_, bufnr)
 	end
 
 	nmap("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
-	nmap("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]tion")
+	nmap("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
 
 	nmap("gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
 	nmap("gi", vim.lsp.buf.implementation, "[G]oto [I]mplementation")
@@ -68,7 +68,7 @@ nvim_lsp.sumneko_lua.setup({
 		Lua = {
 			diagnostics = { globals = { "vim" } },
 		},
-    telemetry = { enable = false, },
+		telemetry = { enable = false },
 	},
 })
 
