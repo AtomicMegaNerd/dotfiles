988f1bba45f8fa4dafb8c74b985694a275dbac5f
Chris Dunphy <chris@megaparsec.ca>
2022-11-27 20:54:15 -0700

    Add PowerShell config for Windows

---
+++ Powershell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,11 @@
+# Oh My Posh
+oh-my-posh init pwsh | Invoke-Expression
+
+# Terminal-Icons
+Import-Module -Name Terminal-Icons
+
+# Aliases
+New-Alias -Name vim -Value nvim
+
+# Functions
+function cdot { cd "C:\Users\chris\Code\Configs\dotfiles" }
