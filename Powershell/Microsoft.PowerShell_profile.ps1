2bd8e66323aa7ea8087b97ab9caa4585a7f24d88
Chris Dunphy <chris@megaparsec.ca>
2022-11-27 21:01:43 -0700

    More fun with Powershell on Windows

--- Powershell/Microsoft.PowerShell_profile.ps1
+++ Powershell/Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,6 @@
+# set PowerShell to UTF-8
+[console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding
+
 # Oh My Posh
 oh-my-posh init pwsh | Invoke-Expression
 
@@ -9,3 +12,8 @@ New-Alias -Name vim -Value nvim
 
 # Functions
 function cdot { cd "C:\Users\chris\Code\Configs\dotfiles" }
+
+function which ($command) {
+  Get-Command -Name $command -ErrorAction SilentlyContinue |
+    Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
+}
