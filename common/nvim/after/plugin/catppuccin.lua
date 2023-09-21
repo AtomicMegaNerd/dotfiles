5e344e19a37a615b9b9f39ec9ba60e5777025558
Chris Dunphy <chris.dunphy@fglsports.com>
2023-09-21 14:42:34 -0600

    No italics

--- common/nvim/after/plugin/catppuccin.lua
+++ common/nvim/after/plugin/catppuccin.lua
@@ -1,3 +1,4 @@
 require("catppuccin").setup({
-    flavour = "latte", -- latte, frappe, macchiato, mocha
+	flavour = "latte", -- latte, frappe, macchiato, mocha
+	no_italic = true, -- Force no italic
 })
