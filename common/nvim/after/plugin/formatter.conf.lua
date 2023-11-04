de383a4087f0c56c286fb5a0c1e1709779402da4
Chris Dunphy <chris@megaparsec.ca>
2023-11-04 00:54:08 +0000

    Using LSP for more formatting
    For example using Ruff instead of Black

--- common/nvim/after/plugin/formatter.conf.lua
+++ common/nvim/after/plugin/formatter.conf.lua
@@ -1,27 +1,18 @@
 local fmt_status, fmt = pcall(require, "formatter")
 if not fmt_status then
-	return
+  return
 end
 
 fmt.setup({
-	filetype = {
-		python = {
-			require("formatter.filetypes.python").black,
-		},
-		lua = {
-			require("formatter.filetypes.lua").stylua,
-		},
-		nix = {
-			require("formatter.filetypes.nix").nixpkgs_fmt,
-		},
-		go = {
-			require("formatter.filetypes.go").gofmt,
-		},
-		json = {
-			require("formatter.filetypes.json").prettier,
-		},
-        yaml = {
-            require("formatter.filetypes.yaml").prettier,
-        },
-	},
+  filetype = {
+    nix = {
+      require("formatter.filetypes.nix").nixpkgs_fmt,
+    },
+    json = {
+      require("formatter.filetypes.json").prettier,
+    },
+    yaml = {
+      require("formatter.filetypes.yaml").prettier,
+    },
+  },
 })
