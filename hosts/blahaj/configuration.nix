583d260c12e0c8892c65b447f4614b8be7290846
Chris Dunphy <chris@megaparsec.ca>
2023-11-08 09:29:58 -0700

    Remove obsolete grub field from config

--- hosts/blahaj/configuration.nix
+++ hosts/blahaj/configuration.nix
@@ -11,7 +11,6 @@
 
   # Use the GRUB 2 boot loader.
   boot.loader.grub.enable = true;
-  boot.loader.grub.version = 2;
   boot.loader.grub.efiSupport = true;
   boot.loader.efi.efiSysMountPoint = "/boot";
   boot.loader.grub.device = "/dev/nvme0n1";
