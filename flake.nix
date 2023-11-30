453df06a1d8822a43dbb1110afa22fce7ec4830f
Chris Dunphy <chris@megaparsec.ca>
2023-11-30 09:36:33 -0700

    Update to nix 23.11

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     # NixOS stable and unstable sources
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
     # Home Manager sources
