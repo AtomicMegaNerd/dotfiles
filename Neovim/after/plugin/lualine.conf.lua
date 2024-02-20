7a6afdb9f0810fec4b1dd6a9b7ee9142fae4e180
Chris Dunphy <chris.dunphy@fglsports.com>
2024-02-20 15:45:53 -0700

    Enable the fugitive extension for lualine

--- Neovim/after/plugin/lualine.conf.lua
+++ Neovim/after/plugin/lualine.conf.lua
@@ -1,13 +1,14 @@
 local status, lualine = pcall(require, "lualine")
 if not status then
-  vim.notify("Cannot load lualine", vim.log.levels.ERROR)
-  return
+	vim.notify("Cannot load lualine", vim.log.levels.ERROR)
+	return
 end
 
 lualine.setup({
-  options = {
-    theme = "catppuccin",
-    icons_enabled = false,
-    globalstatus = true,
-  },
+	options = {
+		theme = "catppuccin",
+		icons_enabled = false,
+		globalstatus = true,
+	},
+	extensions = { "fugitive" },
 })
