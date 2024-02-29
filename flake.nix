f661ea73ac9176155cf408d2495bc82ca3ea6bba
Chris Dunphy <chris@megaparsec.ca>
2024-02-29 15:20:57 -0700

    Simplified flake and switched to unstable
    We are now using unstable everywhere

--- flake.nix
+++ flake.nix
@@ -2,80 +2,55 @@
   description = "AtomicMegaNerd Core Nix Flake";
 
   inputs = {
-    # NixOS stable and unstable sources
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
-
-    # Home Manager sources
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs-unstable";
-
-    # NixOS-WSL
     nixos-wsl = {
       url = "github:nix-community/NixOS-WSL";
       inputs.nixpkgs.follows = "nixpkgs-unstable";
     };
   };
 
-  outputs = { self, nixpkgs, nixpkgs-unstable, nixos-wsl, home-manager }:
+  outputs = { self, nixpkgs-unstable, nixos-wsl, home-manager }:
     let
-      pkgs-linux = import nixpkgs {
-        system = "x86_64-linux";
-        config.allowUnfree = true;
-      };
-      unstable-linux = import nixpkgs-unstable {
-        system = "x86_64-linux";
-        config.allowUnfree = true;
-      };
-      unstable-mac = import nixpkgs-unstable {
-        system = "aarch64-darwin";
+      system-linux = "x86_64-linux";
+      system-mac = "aarch64-darwin";
+      unstable = system: import nixpkgs-unstable {
+        inherit system;
         config.allowUnfree = true;
       };
     in
     {
-
-      # Nix OS core configuration
       nixosConfigurations = {
-        blahaj = nixpkgs.lib.nixosSystem {
-          pkgs = pkgs-linux;
-          modules = [
-            ./hosts/blahaj/configuration.nix
-          ];
+        blahaj = nixpkgs-unstable.lib.nixosSystem {
+          pkgs = unstable system-linux;
+          modules = [ ./hosts/blahaj/configuration.nix ];
         };
         metropolitan = nixpkgs-unstable.lib.nixosSystem {
-          pkgs = unstable-linux;
-          modules = [
-            ./hosts/metropolitan/configuration.nix
-            nixos-wsl.nixosModules.wsl
-          ];
+          pkgs = unstable system-linux;
+          modules = [ ./hosts/metropolitan/configuration.nix nixos-wsl.nixosModules.wsl ];
         };
       };
 
-      # Home Manager configuration
       homeConfigurations = {
-        useGlobalPkgs = true;
-        useUserPackages = true;
-        # Blahaj
         "rcd@blahaj" = home-manager.lib.homeManagerConfiguration {
-          pkgs = unstable-linux;
+          pkgs = unstable system-linux;
           modules = [ ./hosts/blahaj/rcd.nix ];
         };
         "root@blahaj" = home-manager.lib.homeManagerConfiguration {
-          pkgs = unstable-linux;
+          pkgs = unstable system-linux;
           modules = [ ./hosts/blahaj/root.nix ];
         };
-        # Discovery
         "rcd@Discovery" = home-manager.lib.homeManagerConfiguration {
-          pkgs = unstable-mac;
+          pkgs = unstable system-mac;
           modules = [ ./hosts/discovery/rcd.nix ];
         };
-        # Metropolitan
         "rcd@metropolitan" = home-manager.lib.homeManagerConfiguration {
-          pkgs = unstable-linux;
+          pkgs = unstable system-linux;
           modules = [ ./hosts/metropolitan/rcd.nix ];
         };
         "root@metropolitan" = home-manager.lib.homeManagerConfiguration {
-          pkgs = unstable-linux;
+          pkgs = unstable system-linux;
           modules = [ ./hosts/metropolitan/root.nix ];
         };
       };
