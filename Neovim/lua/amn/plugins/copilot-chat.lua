d132762842ea3694216b443f35b1a931d7d5dc25
Chris Dunphy <chris.dunphy@fglsports.com>
2024-03-19 12:23:51 -0600

    Add co-pilot-chat

---
+++ Neovim/lua/amn/plugins/copilot-chat.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    dependencies = {
+      "github/copilot.vim",
+       "nvim-lua/plenary.nvim",
+    },
+    opts = {
+      debug = true, -- Enable debugging
+      -- See Configuration section for rest
+    },
+    -- See Commands section for default commands if you want to lazy load on them
+  },
+}
