9f49f5bf1d1bf3f9ae53caa68b6ded6cafd53ad0
Chris Dunphy <chris@megaparsec.ca>
2024-04-07 09:56:43 -0600

    Disabled ports we no longer need

--- hosts/blahaj/configuration.nix
+++ hosts/blahaj/configuration.nix
@@ -18,8 +18,8 @@
   networking = {
     hostName = "blahaj";
     networkmanager.enable = true;
-    firewall.allowedTCPPorts = [ 8443 8080 8081 53 ];
-    firewall.allowedUDPPorts = [ 3478 53 ];
+    firewall.allowedTCPPorts = [ 8081 53 ];
+    firewall.allowedUDPPorts = [ 53 ];
   };
         
   time.timeZone = "America/Edmonton";
