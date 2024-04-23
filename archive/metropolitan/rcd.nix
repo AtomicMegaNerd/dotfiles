70dea09bcbb614ecaa7f7b4f947fd6946229e1a5
Chris Dunphy <chris@megaparsec.ca>
2024-04-23 09:56:07 -0600

    Move back to WSL for metropolitan
    However archive the old experimental config with Hyprland

--- archive/metropolitan/rcd.nix
+++ archive/metropolitan/rcd.nix
@@ -1,5 +1,4 @@
-{ config, pkgs, ... }:
-{
+{ pkgs, ... }: {
   home.username = "rcd";
   home.homeDirectory = "/home/rcd";
   home.stateVersion = "22.11";
@@ -44,27 +43,14 @@
     userName = "Chris Dunphy";
     userEmail = "chris@megaparsec.ca";
     extraConfig = {
-      core.sshCommand = "ssh.exe";
       init.defaultBranch = "main";
       pull.rebase = false;
-      user.signingkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK9DWvFVS2L2P6G/xUlV0yp6gOpqGgCj4dbY91zyT8ul";
-      gpg = {
-        ssh = {
-          program = "/mnt/c/Program Files/1Password/app/8/op-ssh-sign.exe";
-        };
-        format = "ssh";
-      };
-      commit = {
-        gpgsign = true;
-      };
     };
   };
 
   programs.bat = {
     enable = true;
-    config = {
-      theme = "Nord";
-    };
+    config = { theme = "Nord"; };
   };
 
   programs.fzf = {
@@ -72,10 +58,41 @@
     enableFishIntegration = true;
   };
 
-  xdg.configFile = {
-    nvim = {
-      source = ../../common/nvim;
-      target = "nvim";
+  programs.ssh = {
+    enable = true;
+    forwardAgent = true;
+    extraConfig = ''
+      Host *
+              IdentityAgent ~/.1password/agent.sock
+    '';
+  };
+
+  xdg = {
+
+    mimeApps = {
+      enable = true;
+      associations.added = { "text/html" = "firefox.desktop"; };
+      defaultApplications = { "text/html" = "firefox.desktop"; };
+    };
+
+    configFile = {
+      nvim = {
+        source = ../../common/nvim;
+        target = "nvim";
+      };
+      hypr = {
+        source = ../../common/hypr;
+        target = "hypr";
+      };
+      waybar = {
+        source = ../../common/waybar;
+        target = "waybar";
+      };
+
+      alacritty = {
+        source = ../../common/alacritty;
+        target = "alacritty";
+      };
     };
   };
 }
