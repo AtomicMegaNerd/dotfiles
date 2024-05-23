88f18d0e44e9de6be3b55b2859b1721f36a080ed
Chris Dunphy <chris@megaparsec.ca>
2024-05-22 22:16:39 -0600

    Moar updates

---
+++ common/nvim/after/plugin/copilot-chat.lua
@@ -0,0 +1,7 @@
+local status, copilot_chat = pcall(require, "CopilotChat")
+if not status then
+	vim.notify("Cannot load CopilotChat.nvim", vim.log.levels.ERROR)
+	return
+end
+
+copilot_chat.setup()
