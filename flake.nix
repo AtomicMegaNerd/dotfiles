988342621aa2b3873df71911745a39596ac04a9c
Chris Dunphy <chris@megaparsec.ca>
2024-05-23 17:14:24 -0600

    Update flake

--- flake.nix
+++ flake.nix
@@ -1,23 +1,25 @@
 {
   inputs = {
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs-unstable, home-manager }:
+  outputs = { self, nixpkgs, home-manager }:
     let
       system-linux = "x86_64-linux";
       system-mac = "aarch64-darwin";
 
       pkgs = system:
-        import nixpkgs-unstable {
+        import nixpkgs {
           inherit system;
           config.allowUnfree = true;
         };
 
       nixos = system: hostname:
-        nixpkgs-unstable.lib.nixosSystem {
+        nixpkgs.lib.nixosSystem {
           pkgs = pkgs system;
           modules = [ ./hosts/${hostname}/configuration.nix ];
         };
