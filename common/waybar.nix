3471e9d6ac126ecf9141be76e88faef0ba3b6586
Chris Dunphy <chris@megaparsec.ca>
2024-05-24 16:10:05 -0600

    Update waybar

--- common/waybar.nix
+++ common/waybar.nix
@@ -4,4 +4,10 @@
     enable = true;
     flavor = "frappe";
   };
+  settings = {
+    layer = "top";
+    modules-left = [ "hyprland/workspaces" ];
+    modules-center = [ "hyprland/window" ];
+    modules-right = [ "pulseaudio" "memory" "cpu" "clock" "tray" ];
+  };
 }
