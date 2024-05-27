3ad950df0472f0f2efc4a34ffc0eaa195fb6c65c
Chris Dunphy <chris@megaparsec.ca>
2024-05-27 15:07:53 -0600

    Match the mac for keymaps

--- common/hyprland.nix
+++ common/hyprland.nix
@@ -16,8 +16,10 @@
     "$launcher" = "walker";
 
     # Default mod key
-    "$mod" = "SUPER";
-    "$shiftmod" = "SHIFT_SUPER";
+    "$alt" = "alt";
+    "$shift_alt" = "shift_alt";
+    "$super" = "super";
+    "$shiftsuper" = "shift_super";
 
     # Execute apps at launch
     exec-once = [
@@ -33,40 +35,40 @@
 
     # Bindings
     bind = [
-      "$mod, M, exit"
-      "$mod, Q, killactive"
-      "$mod, F, exec, thunar"
-      "$mod, U, togglefloating"
-      "$mod, T, exec, $terminal"
+      "$super, F, exec, thunar"
+      "$super, T, exec, $terminal"
+      "$super, B, exec, $browser"
+      "$super, space, exec, $launcher"
+      "$alt, X, exit"
+      "$alt, Q, killactive"
+      "$alt, U, togglefloating"
+      "$alt, L, exec, loginctl lock-session"
       "$shiftmod, S, exec, grim"
-      "$mod, B, exec, $browser"
-      "$mod, space, exec, $launcher"
-      "$mod, L, exec, loginctl lock-session"
       "ctrl, right, workspace, e+1"
-      "$mod, left, movefocus, l"
-      "$mod, right, movefocus, r"
-      "$mod, up, movefocus, u"
-      "$mod, down, movefocus, d"
-      "$mod, 1, workspace, 1"
-      "$mod, 2, workspace, 2"
-      "$mod, 3, workspace, 3"
-      "$mod, 4, workspace, 4"
-      "$mod, 5, workspace, 5"
-      "$mod, 6, workspace, 6"
-      "$mod, 7, workspace, 7"
-      "$mod, 8, workspace, 8"
-      "$mod, 9, workspace, 9"
-      "$mod, 0, workspace, 10"
-      "$mod SHIFT, 1, movetoworkspace, 1"
-      "$mod SHIFT, 2, movetoworkspace, 2"
-      "$mod SHIFT, 3, movetoworkspace, 3"
-      "$mod SHIFT, 4, movetoworkspace, 4"
-      "$mod SHIFT, 5, movetoworkspace, 5"
-      "$mod SHIFT, 6, movetoworkspace, 6"
-      "$mod SHIFT, 7, movetoworkspace, 7"
-      "$mod SHIFT, 8, movetoworkspace, 8"
-      "$mod SHIFT, 9, movetoworkspace, 9"
-      "$mod SHIFT, 0, movetoworkspace, 10"
+      "$alt, left, movefocus, l"
+      "$alt, right, movefocus, r"
+      "$alt, up, movefocus, u"
+      "$alt, down, movefocus, d"
+      "$alt, 1, workspace, 1"
+      "$alt, 2, workspace, 2"
+      "$alt, 3, workspace, 3"
+      "$alt, 4, workspace, 4"
+      "$alt, 5, workspace, 5"
+      "$alt, 6, workspace, 6"
+      "$alt, 7, workspace, 7"
+      "$alt, 8, workspace, 8"
+      "$alt, 9, workspace, 9"
+      "$alt, 0, workspace, 10"
+      "$alt SHIFT, 1, movetoworkspace, 1"
+      "$alt SHIFT, 2, movetoworkspace, 2"
+      "$alt SHIFT, 3, movetoworkspace, 3"
+      "$alt SHIFT, 4, movetoworkspace, 4"
+      "$alt SHIFT, 5, movetoworkspace, 5"
+      "$alt SHIFT, 6, movetoworkspace, 6"
+      "$alt SHIFT, 7, movetoworkspace, 7"
+      "$alt SHIFT, 8, movetoworkspace, 8"
+      "$alt SHIFT, 9, movetoworkspace, 9"
+      "$alt SHIFT, 0, movetoworkspace, 10"
     ];
 
     general = {
