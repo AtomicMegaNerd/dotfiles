9b6751d6ee9ad70c90e3d2f3b48a319c337b89f9
Chris Dunphy <chris.dunphy@fglsports.com>
2024-05-28 14:07:41 -0600

    Neogit

---
+++ Neovim/lua/amn/plugins/neogit.lua
@@ -0,0 +1,29 @@
+return {
+	"neogitorg/neogit",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"sindrets/diffview.nvim",
+		"nvim-telescope/telescope.nvim",
+		"ibhagwan/fzf-lua",
+	},
+	config = function()
+		local utils = require("amn.utils")
+		if not utils then
+			vim.notify("Utils not found", vim.log.levels.ERROR)
+			return
+		end
+
+		local neogit = utils.do_import("neogit")
+		if not neogit then
+			return
+		end
+
+		neogit.setup({
+			integrations = {
+				diffview = true,
+				telescope = true,
+				fzf_lua = true,
+			},
+		})
+	end,
+}
