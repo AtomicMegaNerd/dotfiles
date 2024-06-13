e11dcfa253c365ad956e933beb66305099f2c65b
Chris Dunphy <chris@megaparsec.ca>
2024-06-13 03:00:55 +0000

    Add Dockerfile to treesitter

--- hosts/metropolitan/configuration.nix
+++ hosts/metropolitan/configuration.nix
@@ -26,7 +26,7 @@ in {
     podman = {
       enable = true;
       dockerCompat = true;
-
+      dockerSocket.enable = true;
       defaultNetwork.settings.dns_enabled = true;
     };
   };
--- nix/neovim.nix
+++ nix/neovim.nix
@@ -22,6 +22,7 @@
         p.toml
         p.zig
         p.hyprlang
+        p.dockerfile
       ]))
     ];
 }
