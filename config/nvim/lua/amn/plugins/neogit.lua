fd588e51a42de34b6ce224b90b5f6a20f7bf1d45
Chris Dunphy <chris@megaparsec.ca>
2024-06-13 03:07:52 +0000

    Enable all treesitter grammares

--- config/nvim/lua/amn/plugins/neogit.lua
+++ config/nvim/lua/amn/plugins/neogit.lua
@@ -1,11 +1,9 @@
 return {
 	"NeogitOrg/neogit",
 	dependencies = {
-		"nvim-lua/plenary.nvim", -- required
-		"sindrets/diffview.nvim", -- optional - Diff integration
-
-		-- Only one of these is needed, not both.
-		"nvim-telescope/telescope.nvim", -- optional
+		"nvim-lua/plenary.nvim",
+		"sindrets/diffview.nvim",
+		"nvim-telescope/telescope.nvim",
 	},
 	config = function()
 		local utils = require("amn.utils")
--- nix/neovim.nix
+++ nix/neovim.nix
@@ -5,24 +5,6 @@
 
   plugins = with pkgs.vimPlugins;
     [
-      (nvim-treesitter.withPlugins (p: [
-        p.c
-        p.lua
-        p.nix
-        p.go
-        p.gomod
-        p.rust
-        p.haskell
-        p.python
-        p.typescript
-        p.fish
-        p.bash
-        p.markdown
-        p.yaml
-        p.toml
-        p.zig
-        p.hyprlang
-        p.dockerfile
-      ]))
+      nvim-treesitter.withAllGrammars
     ];
 }
