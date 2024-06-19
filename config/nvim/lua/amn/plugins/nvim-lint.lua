fd6923a5de0c01f4bfdab65c33383af2e252f33b
Chris Dunphy <chris@megaparsec.ca>
2024-06-19 08:31:19 -0600

    Add some new plug-ins

---
+++ config/nvim/lua/amn/plugins/nvim-lint.lua
@@ -0,0 +1,12 @@
+return {
+	"mfussenegger/nvim-lint",
+	config = function()
+		local utils = require("amn.utils")
+		local lint = utils.do_import("lint")
+		if not lint then
+			return
+		end
+
+		lint.linters_by_ft = { go = "golangci-lint" }
+	end,
+}
---
+++ config/nvim/lua/amn/plugins/oil.lua
@@ -0,0 +1,14 @@
+return {
+	"stevearc/oil",
+	config = function()
+		local utils = require("amn.utils")
+		local oil = utils.do_import("oil")
+		if not oil then
+			return
+		end
+		oil.setup()
+
+		-- Keymaps
+		vim.keymap.set("n", "-", "<Cmd>Oil<CR>", { desc = "Open current directory in Oil" })
+	end,
+}
