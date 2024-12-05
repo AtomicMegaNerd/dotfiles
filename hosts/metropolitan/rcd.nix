94731fa42b398185d633d9a825df0e8403b45751
Chris Dunphy <chris@megaparsec.ca>
2024-12-05 13:11:58 -0700

    Fix Zellij configuration

--- hosts/metropolitan/rcd.nix
+++ hosts/metropolitan/rcd.nix
@@ -62,5 +62,9 @@ in {
       source = ../../config/nvim;
       target = "nvim";
     };
+    zellij = {
+      source = ../../config/zellij/nix;
+      target = "zellij";
+    };
   };
 }
--- nix/zellij.nix
+++ nix/zellij.nix
@@ -1,11 +1,5 @@
 {
   enable = true;
-  settings = {
-    pane_frames = false;
-    default_mode = "locked";
-    serialization_interval = 1;
-  };
-
   catppuccin = {
     enable = true;
     flavor = "macchiato";
