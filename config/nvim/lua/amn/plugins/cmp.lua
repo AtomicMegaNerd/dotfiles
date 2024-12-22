f14095f88c25f758fd9d44b65bd8661bab6935a8
Chris Dunphy <chris@megaparsec.ca>
2024-12-21 20:25:20 -0700

    Update flake

--- config/nvim/lua/amn/plugins/cmp.lua
+++ config/nvim/lua/amn/plugins/cmp.lua
@@ -1,20 +1,20 @@
 return {
 	"saghen/blink.cmp",
 
-	version = "v0.*",
+	version = "*",
+
 	opts = {
 		keymap = { preset = "default" },
 
 		appearance = {
-			use_nvim_cmp_as_default = true,
-			nerd_font_variant = "mono",
+			nerd_font_variant = "normal",
 		},
 
+		-- Default list of enabled providers defined so that you can extend it
+		-- elsewhere in your config, without redefining it, due to `opts_extend`
 		sources = {
 			default = { "lsp", "path", "buffer" },
 		},
-
-		signature = { enabled = true },
 	},
 	opts_extend = { "sources.default" },
 }
