249c3368a4930d3efd7746dfe9d8bd9457387c4e
Chris Dunphy <chris@megaparsec.ca>
2024-12-22 17:27:45 -0700

    Fix blink.cmp

--- config/nvim/lua/amn/plugins/cmp.lua
+++ config/nvim/lua/amn/plugins/cmp.lua
@@ -10,11 +10,10 @@ return {
 			nerd_font_variant = "normal",
 		},
 
-		-- Default list of enabled providers defined so that you can extend it
-		-- elsewhere in your config, without redefining it, due to `opts_extend`
 		sources = {
 			default = { "lsp", "path", "buffer" },
 		},
 	},
-	opts_extend = { "sources.default" },
+
+	signature = { enabled = true },
 }
