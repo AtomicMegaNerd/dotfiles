8d1a69ef0fea3bd3ea2e0c4d0c0055390a9acac1
Chris Dunphy <chris@megaparsec.ca>
2024-12-23 17:09:32 -0700

    Fix clipboard for Neovim on Wayland

--- config/nvim/lua/amn/user/linux.lua
+++ config/nvim/lua/amn/user/linux.lua
@@ -10,6 +10,17 @@ local is_wsl = function()
 	return false
 end
 
+local is_wayland = function()
+	local xdg_session_type = vim.fn.getenv("XDG_SESSION_TYPE")
+	if xdg_session_type == nil then
+		return false
+	end
+	if string.find(xdg_session_type, "wayland") then
+		return true
+	end
+	return false
+end
+
 -- If we are in WSL then we want to use the Windows clipboard :-)
 if is_wsl() then
 	vim.g.clipboard = {
@@ -21,6 +32,23 @@ if is_wsl() then
 		paste = {
 			["+"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
 			["*"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+		},
+
+		cache_enabled = 0,
+	}
+end
+
+-- If we are in Wayland then we want to use wl-clipboard :-)
+if is_wayland() then
+	vim.g.clipboard = {
+		name = "WaylandClipboard",
+		copy = {
+			["+"] = "wl-copy",
+			["*"] = "wl-copy",
+		},
+		paste = {
+			["+"] = "wl-paste",
+			["*"] = "wl-paste",
 		},
 
 		cache_enabled = 0,
