6b32e2e5fba82571d564acf96e466771f069d53a
Chris Dunphy <chris@megaparsec.ca>
2024-12-27 14:34:09 -0700

    Don't autocomplete in cmd mode in vim

--- config/nvim/lua/amn/plugins/cmp.lua
+++ config/nvim/lua/amn/plugins/cmp.lua
@@ -1,15 +1,22 @@
 return {
 	"saghen/blink.cmp",
-	version = "v0.8.1",
+	version = "*",
 	opts = {
 		keymap = { preset = "enter" },
 		appearance = {
-			nerd_font_variant = "normal",
+			nerd_font_variant = "mono",
 		},
 		sources = {
 			default = { "lsp", "path", "buffer" },
 		},
 		signature = { enabled = true },
+		completion = {
+			menu = {
+				auto_show = function(ctx)
+					return ctx.mode ~= "cmdline"
+				end,
+			},
+		},
 	},
 	opts_extend = { "sources.default" },
 }
