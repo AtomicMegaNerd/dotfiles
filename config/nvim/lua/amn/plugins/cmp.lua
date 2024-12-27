4c00330ab6c6a9dbb866146e92abfcb2cc6a3a93
Chris Dunphy <chris@megaparsec.ca>
2024-12-27 13:27:14 -0700

    Update flake and change blink.cmp keymap
    Use enter instead of Ctrl-Y to accept

--- config/nvim/lua/amn/plugins/cmp.lua
+++ config/nvim/lua/amn/plugins/cmp.lua
@@ -2,7 +2,7 @@ return {
 	"saghen/blink.cmp",
 	version = "v0.8.1",
 	opts = {
-		keymap = { preset = "default" },
+		keymap = { preset = "enter" },
 		appearance = {
 			nerd_font_variant = "normal",
 		},
