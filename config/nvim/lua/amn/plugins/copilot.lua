178dd5117898f064974de436b2d33c8c9e5d90a1
Chris Dunphy <chris@megaparsec.ca>
2024-12-27 19:31:41 -0700

    Update copilot to notify when enabling or disabling

--- config/nvim/lua/amn/plugins/copilot.lua
+++ config/nvim/lua/amn/plugins/copilot.lua
@@ -1,8 +1,21 @@
 return {
 	"github/copilot.vim",
 	init = function()
-		vim.g.copilot_assume_mapped = true
-		vim.g.copilot_proxy_strict_ssl = false
 		vim.g.copilot_enabled = false
+
+		local utils = require("amn.utils")
+
+		local function enable_copilot()
+			vim.cmd("Copilot enable")
+			vim.notify("Copilot enabled", vim.log.levels.INFO)
+		end
+
+		local function disable_copilot()
+			vim.cmd("Copilot disable")
+			vim.notify("Copilot disabled", vim.log.levels.INFO)
+		end
+
+		utils.nmap("<leader>pe", enable_copilot, "Co-[P]ilot [E]nabled")
+		utils.nmap("<leader>pd", disable_copilot, "Co-[P]ilot [D]isabled")
 	end,
 }
