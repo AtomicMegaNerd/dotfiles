7fb90ecea68c6a92c912b93ef556e2c9b6bc0c02
Chris Dunphy <chris@megaparsec.ca>
2024-12-29 10:03:30 -0700

    A few more Neovim updates

--- config/nvim/lua/amn/plugins/oil.lua
+++ config/nvim/lua/amn/plugins/oil.lua
@@ -6,13 +6,12 @@ return {
 		if not oil then
 			return
 		end
+
 		oil.setup({
 			view_options = {
 				show_hidden = true,
 			},
 		})
-
-		-- Keymaps
 		vim.keymap.set("n", "<leader>o", "<Cmd>Oil<CR>", { desc = "Open current directory in Oil" })
 	end,
 }
