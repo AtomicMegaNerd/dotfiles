0cfa9ad597bc86b43f471efd071517d9e5a0ddfd
Chris Dunphy <chris@megaparsec.ca>
2025-03-06 13:01:16 -0700

    Update metropolitan

--- hosts/metropolitan/configuration.nix
+++ hosts/metropolitan/configuration.nix
@@ -1,43 +1,28 @@
 { pkgs, ... }:
 let rcd_pub_key = builtins.readFile ../../static/rcd_pub_key;
 in {
-  imports = [ # Include the results of the hardware scan.
-    ./hardware-configuration.nix
-  ];
+  imports = [ ./hardware-configuration.nix ];
 
-  # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
-  networking.hostName = "metropolitan"; # Define your hostname.
-
-  # Enable networking
+  networking.hostName = "metropolitan";
   networking.networkmanager.enable = true;
 
-  # Set your time zone.
   time.timeZone = "America/Edmonton";
-
-  # Select internationalisation properties.
   i18n.defaultLocale = "en_CA.UTF-8";
 
-  # Enable the X11 windowing system.
   services.xserver.enable = true;
-
-  # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
-
-  # Configure keymap in X11
   services.xserver.xkb = {
     layout = "us";
     variant = "";
   };
 
-  # Enable CUPS to print documents.
   services.printing.enable = true;
 
-  # Enable sound with pipewire.
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
@@ -47,7 +32,6 @@ in {
     pulse.enable = true;
   };
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.rcd = {
     isNormalUser = true;
     description = "Chris Dunphy";
@@ -56,12 +40,9 @@ in {
     openssh.authorizedKeys.keys = [ rcd_pub_key ];
   };
 
-  # Install firefox.
   programs.firefox.enable = true;
   programs.fish.enable = true;
   programs.steam.enable = true;
-
-  # 1Password
   programs._1password.enable = true;
   programs._1password-gui = {
     enable = true;
@@ -78,6 +59,7 @@ in {
     wl-clipboard
     bottles
     gnome-tweaks
+    gnomeExtensions.appindicator
   ];
 
   # GNOME exclusions
@@ -119,5 +101,4 @@ in {
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "24.11"; # Did you read the comment?
-
 }
