aade5b587d451091e93348117f309ed107aa315a
Chris Dunphy <chris.dunphy@fglsports.com>
2025-03-06 11:57:14 -0700

    Update fish

--- config/fish/workmbp/config.fish
+++ config/fish/workmbp/config.fish
@@ -79,6 +79,10 @@ status is-interactive; and zoxide init fish --cmd cd | source
 # ===================================
 status is-interactive; and fzf --fish | source
 
+# Pyenv
+# ===================================
+status is-interactive; and pyenv init - fish | source
+
 # Direnv
 # ===================================
 direnv hook fish | source
