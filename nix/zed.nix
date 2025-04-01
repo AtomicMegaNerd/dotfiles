0c680f5c9c9e20782e6d50c4add0c12fcef4b3fd
Chris Dunphy <chris@megaparsec.ca>
2025-04-01 12:52:46 -0600

    Fix Zed config

--- nix/zed.nix
+++ nix/zed.nix
@@ -1,4 +1,12 @@
 {
   enable = true;
-  userSettings = { ui_font_size = 18; };
+  userSettings = {
+    vim_mode = true;
+    ui_font_size = 24;
+    buffer_font_size = 24;
+    theme = {
+      mode = "dark";
+      dark = "Catppuccin Macchiato";
+    };
+  };
 }
