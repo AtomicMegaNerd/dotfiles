9963dcde3d2fbdf79a368cbd9cccefa0858c6f40
Chris Dunphy <chris@megaparsec.ca>
2025-04-01 12:25:28 -0600

    Get rid of fractional scaling

--- hosts/metropolitan/configuration.nix
+++ hosts/metropolitan/configuration.nix
@@ -18,14 +18,7 @@ in {
     displayManager.gdm = {
       enable = true;
       wayland = true;
-    };
-    desktopManager.gnome = {
-      enable = true;
-      extraGSettingsOverridePackages = [ pkgs.mutter ];
-      extraGSettingsOverrides = ''
-        [org.gnome.mutter]
-        experimental-features=['scale-monitor-framebuffer']
-      '';
+      gnome.enable = true;
     };
   };
 
