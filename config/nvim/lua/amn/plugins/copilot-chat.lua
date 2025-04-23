89c3d5a6d0b693a0a423ccd96d800b9d98a9b881
Chris Dunphy <chris.dunphy@fglsports.com>
2025-04-23 12:29:05 -0600

    Do not include copilot-chat in list of buffers

--- config/nvim/lua/amn/plugins/copilot-chat.lua
+++ config/nvim/lua/amn/plugins/copilot-chat.lua
@@ -1,70 +1,68 @@
 return {
-	"CopilotC-Nvim/CopilotChat.nvim",
-	branch = "main",
-	dependencies = {
-		"github/copilot.vim",
-		"nvim-lua/plenary.nvim",
-	},
-	config = function()
-		local utils = require("amn.utils")
-		local chat = utils.do_import("CopilotChat")
-		local select = utils.do_import("CopilotChat.select")
+  "CopilotC-Nvim/CopilotChat.nvim",
+  branch = "main",
+  dependencies = {
+    "github/copilot.vim",
+    "nvim-lua/plenary.nvim",
+  },
+  config = function()
+    local utils = require("amn.utils")
+    local chat = utils.do_import("CopilotChat")
+    local select = utils.do_import("CopilotChat.select")
 
-		if not chat or not select then
-			return
-		end
+    if not chat or not select then
+      return
+    end
 
-		chat.setup({
-			show_help = false,
-			context = "buffers",
-			selection = select.buffer,
-			chat_autocomplete = true,
-			-- This hack is needed to make Tab work for autocomplete in the chat window
-			mappings = {
-				complete = {
-					insert = "<C-t>",
-				},
-			},
-		})
+    chat.setup({
+      show_help = false,
+      context = "buffers",
+      selection = select.buffer,
+      chat_autocomplete = true,
+      -- This hack is needed to make Tab work for autocomplete in the chat window
+      mappings = {
+        complete = {
+          insert = "<C-t>",
+        },
+      },
+    })
 
-		utils.nmap("<leader>pc", function()
-			chat.open({
-				window = {
-					title = "Co-Pilot Chat",
-					layout = "replace",
-				},
-			})
-			-- This ensures that the buffer shows up in the buffer list
-			vim.cmd("setlocal buflisted")
-		end, "Co[P]ilot [C]hat")
+    utils.nmap("<leader>pc", function()
+      chat.open({
+        window = {
+          title = "Co-Pilot Chat",
+          layout = "replace",
+        },
+      })
+    end, "Co[P]ilot [C]hat")
 
-		utils.nmap("<leader>pt", function()
-			chat.toggle()
-		end, "Co[P]ilot [T]oggle")
+    utils.nmap("<leader>pt", function()
+      chat.toggle()
+    end, "Co[P]ilot [T]oggle")
 
-		utils.nmap("<leader>pf", function()
-			chat.open({
-				window = {
-					title = "Co-Pilot Chat",
-					layout = "float",
-				},
-			})
-		end, "Co[P]ilot [F]loat")
+    utils.nmap("<leader>pf", function()
+      chat.open({
+        window = {
+          title = "Co-Pilot Chat",
+          layout = "float",
+        },
+      })
+    end, "Co[P]ilot [F]loat")
 
-		utils.nvmap("<leader>pbe", function()
-			chat.ask("Please explain how this works", { selection = select.buffer })
-		end, "Co[P]ilot [B]uffer [E]xplain")
+    utils.nvmap("<leader>pbe", function()
+      chat.ask("Please explain how this works", { selection = select.buffer })
+    end, "Co[P]ilot [B]uffer [E]xplain")
 
-		utils.vmap("<leader>pse", function()
-			chat.ask("Please explain how this works", { selection = select.visual })
-		end, "Co[P]ilot [S]election [E]xplain")
+    utils.vmap("<leader>pse", function()
+      chat.ask("Please explain how this works", { selection = select.visual })
+    end, "Co[P]ilot [S]election [E]xplain")
 
-		utils.nvmap("<leader>pob", function()
-			chat.ask("Please optimize this code", { selection = select.buffer })
-		end, "Co[P]ilot [O]ptimize [B]uffer")
+    utils.nvmap("<leader>pob", function()
+      chat.ask("Please optimize this code", { selection = select.buffer })
+    end, "Co[P]ilot [O]ptimize [B]uffer")
 
-		utils.vmap("<leader>pos", function()
-			chat.ask("Please optimize this code", { selection = select.visual })
-		end, "Co[P]ilot [O]ptimize [S]election")
-	end,
+    utils.vmap("<leader>pos", function()
+      chat.ask("Please optimize this code", { selection = select.visual })
+    end, "Co[P]ilot [O]ptimize [S]election")
+  end,
 }
