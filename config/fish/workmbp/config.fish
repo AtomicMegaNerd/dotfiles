02de339406750d3a41ae80939ecab06d032a555b
Chris Dunphy <chris.dunphy@fglsports.com>
2025-04-30 13:18:08 -0600

    Fix work configs
    Clean up work config.fish and make separate workmbp ghostty config

--- config/fish/workmbp/config.fish
+++ config/fish/workmbp/config.fish
@@ -9,11 +9,6 @@ if status is-interactive
     alias cat "bat --paging=never --style=plain"
     alias df "duf"
 
-    # Shortcuts for common files and directories
-    alias vconf "nvim ~/.config/nvim/init.lua"
-    alias fconf "nvim ~/.config/fish/config.fish"
-    alias cconf "nvim ~/.config/fish/conf.d/credentials.fish"
-
     # Terraform
     alias tf terraform
 
@@ -31,40 +26,47 @@ if status is-interactive
     function zn
         zellij -s (basename (eval pwd))
     end
+
+    # Starship prompt
+    # ===================================
+    starship init fish | source
+
+    # Zoxide
+    # ===================================
+    zoxide init fish --cmd cd | source
+
+    # Fzf
+    # ===================================
+    fzf --fish | source
+
+    # Direnv
+    # ===================================
+    direnv hook fish | source
 end
 
-# Variables
+# Go
+# ===================================
+# This lets us use Go modules hosted on Azure DevOps
+set -gx GOPRIVATE dev.azure.com
+# This is a workaround for the issue with Go modules behind our corporate proxy
+set -gx GOPROXY direct
+
+# CLI THEME
+# ===================================
+set -gx CLI_THEME "catppuccin"
+
+# Neovim
+# ===================================
+set -gx EDITOR nvim
+set -gx NVIM_LOG_FILE ~/.local/share/nvim/nvim.log
+
+# Other Variables
 # ===================================
 set fish_greeting # Disable greeting
-set -gx EDITOR nvim
 set -gx FUNCTIONS_CORE_TOOLS_TELEMETRY_OPTOUT 1
 set -gx DOTNET_CLI_TELEMETRY_OPTOUT 1
 set -gx VIRTUAL_ENV_DISABLE_PROMPT 1
-
-# CLI Theme
-# ===================================
-
-set -gx CLI_THEME catppuccin
-
-# Fixes an issue with MacOS' super out of date terminfo
-set -gx TERMINFO_DIRS "$TERMINFO_DIRS:$HOME/.local/share/terminfo"
-
-# Go
-set -gx GOPRIVATE dev.azure.com
-
-# Tell Vault to use JSON output
-set -gx VAULT_FORMAT json
-
 set -gx CERTS $HOME/Certs/
-
-# Deal with corporate proxies
-set -gx NODE_TLS_REJECT_UNAUTHORIZED 0
-
-# Logging for Neovim
-set -gx NVIM_LOG_FILE ~/.local/share/nvim/nvim.log
-
-# Set Docker host path
-set -gx DOCKER_HOST 'unix:///var/folders/bj/77t80hr144bf0n6vrdtw_1t00000gp/T/podman/podman-machine-default-api.sock'
 
 # PATH
 # ===================================
@@ -73,18 +75,3 @@ fish_add_path $HOME/.local/bin
 fish_add_path $HOME/go/bin
 fish_add_path /opt/homebrew/bin
 
-# Starship prompt
-# ===================================
-status is-interactive; and starship init fish | source
-
-# Zoxide
-# ===================================
-status is-interactive; and zoxide init fish --cmd cd | source
-
-# Fzf
-# ===================================
-status is-interactive; and fzf --fish | source
-
-# Direnv
-# ===================================
-direnv hook fish | source
