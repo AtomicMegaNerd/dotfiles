7155b9b303b9d8e2c8b1d4f377478f81910fa507
Chris Dunphy <chris@megaparsec.ca>
2025-05-14 09:12:32 -0600

    Clean up nix config for metropolitan

--- hosts/metropolitan/configuration.nix
+++ hosts/metropolitan/configuration.nix
@@ -1,14 +1,5 @@
-# Edit this configuration file to define what should be installed on
-# your system. Help is available in the configuration.nix(5) man page, on
-# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
-
-# NixOS-WSL specific options are documented on the NixOS-WSL repository:
-# https://github.com/nix-community/NixOS-WSL
-
 { pkgs, ... }:
-
 {
-
   wsl = {
 	  enable = true;
 	  defaultUser = "rcd";
@@ -17,7 +8,7 @@
 	  };
   };
 
-     time.timeZone = "America/Edmonton";
+  time.timeZone = "America/Edmonton";
   i18n.defaultLocale = "en_CA.UTF-8";
 
    users = {
@@ -36,6 +27,7 @@
     curl
     git
     neovim
+    wget
   ];
 
   nix = {
@@ -53,11 +45,5 @@
     flavor = "macchiato";
   };
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It's perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.11"; # Did you read the comment?
+  system.stateVersion = "24.11";
 }
