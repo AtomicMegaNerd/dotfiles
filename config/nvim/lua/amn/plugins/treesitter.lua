c42511e54f73a47f2141051fa2df2435419af13f
Chris Dunphy <chris@megaparsec.ca>
2025-05-31 06:53:45 -0600

    Update nvim-treesitter

--- config/nvim/lua/amn/plugins/treesitter.lua
+++ config/nvim/lua/amn/plugins/treesitter.lua
@@ -1,23 +1,25 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	config = function()
-		local utils = require("amn.utils")
-		local configs = utils.do_import("nvim-treesitter.configs")
+  "nvim-treesitter/nvim-treesitter",
+  branch = "main",
+  lazy = false,
+  build = ":TSUpdate",
+  config = function()
+    local utils = require("amn.utils")
+    local configs = utils.do_import("nvim-treesitter.configs")
 
-		if not configs then
-			return
-		end
+    if not configs then
+      return
+    end
 
-		configs.setup({
-			auto_install = false,
-			highlight = {
-				enable = true,
-			},
-			indent = {
-				enable = true,
-				disable = { "go" },
-			},
-		})
-	end,
+    configs.setup({
+      auto_install = false,
+      highlight = {
+        enable = true,
+      },
+      indent = {
+        enable = true,
+        disable = { "go" },
+      },
+    })
+  end,
 }
