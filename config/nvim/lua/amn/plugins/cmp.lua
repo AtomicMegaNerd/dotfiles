4afe8029a6d3e699e474957138c5942478040145
Chris Dunphy <chris.dunphy@fglsports.com>
2025-09-22 08:30:29 -0600

    Update LSP config

--- config/nvim/lua/amn/plugins/cmp.lua
+++ config/nvim/lua/amn/plugins/cmp.lua
@@ -1,16 +1,20 @@
 return {
   "saghen/blink.cmp",
-  version = "*",
+  version = "1.*",
+
   opts = {
+
     appearance = {
       nerd_font_variant = "mono",
     },
+
     sources = {
       default = { "lsp", "path", "buffer" },
       per_filetype = {
         codecompanion = { "codecompanion" },
       }
     },
+
     signature = { enabled = true },
   },
   opts_extend = { "sources.default" },
--- config/nvim/lua/amn/plugins/lsp.lua
+++ config/nvim/lua/amn/plugins/lsp.lua
@@ -1,57 +1,29 @@
 return {
   "neovim/nvim-lspconfig",
-  dependencies = {
-    "saghen/blink.cmp",
-  },
 
   config = function()
     local utils = require("amn.utils")
+    vim.api.nvim_create_autocmd("LspAttach", {
+      callback = function(args)
+        utils.nmap("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame", "LSP", args.buf)
+        utils.nmap("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction", "LSP", args.buf)
+        utils.nmap("gd", vim.lsp.buf.definition, "[G]oto [D]efinition", "LSP", args.buf)
+        utils.nmap("gl", vim.lsp.buf.declaration, "[G]oto [D]eclaration", "LSP", args.buf)
+        utils.nmap("gi", vim.lsp.buf.implementation, "[G]oto [I]mplementation", "LSP", args.buf)
+        utils.nmap("gt", vim.lsp.buf.type_definition, "Type [D]efinition", "LSP", args.buf)
+        utils.nmap("K", vim.lsp.buf.hover, "Hover Documentation", "LSP", args.buf)
+        utils.nmap("<C-k>", vim.lsp.buf.signature_help, "Signature Documentation", "LSP", args.buf)
+        utils.nmap("<leader>td",
+          function()
+            local current = vim.diagnostic.config().virtual_text
+            vim.diagnostic.config({ virtual_text = not current })
+          end,
+          "[T]oggle [D]iagnostics", "LSP",
+          args.buf)
+      end,
+    })
 
-    local nvim_lsp = utils.do_import("lspconfig")
-    local cmp = utils.do_import("blink.cmp")
-    if not nvim_lsp or not cmp then
-      return
-    end
-
-    local capabilities = cmp.get_lsp_capabilities()
-    if not capabilities then
-      return
-    end
-
-    local on_attach = function(_, bufnr)
-      utils.nmap("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame", "LSP", bufnr)
-      utils.nmap("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction", "LSP", bufnr)
-      utils.nmap("gd", vim.lsp.buf.definition, "[G]oto [D]efinition", "LSP", bufnr)
-      utils.nmap("gl", vim.lsp.buf.declaration, "[G]oto [D]eclaration", "LSP", bufnr)
-      utils.nmap("gi", vim.lsp.buf.implementation, "[G]oto [I]mplementation", "LSP", bufnr)
-      utils.nmap("gt", vim.lsp.buf.type_definition, "Type [D]efinition", "LSP", bufnr)
-      utils.nmap("K", vim.lsp.buf.hover, "Hover Documentation", "LSP", bufnr)
-      utils.nmap("<C-k>", vim.lsp.buf.signature_help, "Signature Documentation", "LSP", bufnr)
-      utils.nmap("<leader>td", function()
-        local current = vim.diagnostic.config().virtual_text
-        vim.diagnostic.config({ virtual_text = not current })
-      end, "[T]oggle [D]iagnostics", "LSP", bufnr)
-    end
-
-    local servers = {
-      "ruff",
-      "nil_ls",
-      "bashls",
-      "gopls",
-      "dockerls",
-      "rust_analyzer",
-    }
-
-    for _, lsp in ipairs(servers) do
-      nvim_lsp[lsp].setup({
-        on_attach = on_attach,
-        capabilities = capabilities,
-      })
-    end
-
-    nvim_lsp.yamlls.setup({
-      on_attach = on_attach,
-      capabilities = capabilities,
+    vim.lsp.config("yamlls", {
       settings = {
         yaml = {
           keyOrdering = false,
@@ -59,9 +31,7 @@ return {
       },
     })
 
-    nvim_lsp.pyright.setup({
-      on_attach = on_attach,
-      capabilities = capabilities,
+    vim.lsp.config("pyright", {
       settings = {
         pyright = {
           -- Using Ruff's import organizer
@@ -76,13 +46,20 @@ return {
       },
     })
 
-    -- Make sure we suppress warnings on the vim global object
-    nvim_lsp.lua_ls.setup({
-      on_attach = on_attach,
-      capabilities = capabilities,
-      settings = {
-        telemetry = { enable = false },
-      },
-    })
+    local servers = {
+      "ruff",
+      "nil_ls",
+      "bashls",
+      "gopls",
+      "dockerls",
+      "rust_analyzer",
+      "lua_ls",
+      "yamlls",
+      "pyright",
+    }
+
+    for _, lsp in ipairs(servers) do
+      vim.lsp.enable(lsp)
+    end
   end,
 }
