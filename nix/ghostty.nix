6d97e40aa4340b2c798b1855fc1536d6d1d099ec
Chris Dunphy <chris@megaparsec.ca>
2025-10-23 13:22:59 -0600

    Fixes

--- nix/ghostty.nix
+++ nix/ghostty.nix
@@ -1,11 +1,9 @@
 { pkgs, ... }:
 {
   enable = true;
-  enableFishIntegration = true;
   package = pkgs.ghostty-bin;
 
   settings = {
-    theme = "Catppuccin Latte";
     font-family = "Monaspace Neon";
     font-size = 14;
     macos-option-as-alt = true;
