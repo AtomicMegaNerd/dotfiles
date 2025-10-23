4a6d028141f187001868508b9f9ad87dd593a647
Chris Dunphy <chris@megaparsec.ca>
2025-10-23 09:20:34 -0600

    Remove neotest

--- config/nvim/lua/amn/plugins/catppuccin.lua
+++ config/nvim/lua/amn/plugins/catppuccin.lua
@@ -2,33 +2,32 @@
 -- we want to load this plug-in.
 
 return {
-	"catppuccin/nvim",
-	name = "catppuccin",
-	priority = 1000,
+  "catppuccin/nvim",
+  name = "catppuccin",
+  priority = 1000,
 
-	config = function()
-		local utils = require("amn.utils")
-		local catppuccin = utils.do_import("catppuccin")
+  config = function()
+    local utils = require("amn.utils")
+    local catppuccin = utils.do_import("catppuccin")
 
-		if not catppuccin then
-			return
-		end
+    if not catppuccin then
+      return
+    end
 
-		catppuccin.setup({
-			flavour = "latte",
-			no_italic = true,
-			transparent_background = true,
-			integrations = {
-				gitsigns = true,
-				fzf = true,
-				snacks = true,
-				which_key = true,
-				trouble = true,
-				neotest = true,
-				neogit = true,
-			},
-		})
+    catppuccin.setup({
+      flavour = "latte",
+      no_italic = true,
+      transparent_background = true,
+      integrations = {
+        gitsigns = true,
+        fzf = true,
+        snacks = true,
+        which_key = true,
+        trouble = true,
+        neogit = true,
+      },
+    })
 
-		vim.cmd.colorscheme("catppuccin")
-	end,
+    vim.cmd.colorscheme("catppuccin")
+  end,
 }
