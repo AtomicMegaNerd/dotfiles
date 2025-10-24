0d6e75774e38de86bc6e597571acebc5bfa08260
Chris Dunphy <chris@megaparsec.ca>
2025-10-24 10:26:53 -0600

    Remove LLM plug-ins from Neovim

--- config/nvim/lua/amn/plugins/cmp.lua
+++ config/nvim/lua/amn/plugins/cmp.lua
@@ -1,17 +1,14 @@
 return {
-  "saghen/blink.cmp",
-  version = "1.*",
-  opts = {
-    appearance = {
-      nerd_font_variant = "mono",
-    },
-    sources = {
-      default = { "lsp", "path", "buffer" },
-      per_filetype = {
-        codecompanion = { "codecompanion" },
-      }
-    },
-    signature = { enabled = true },
-  },
-  opts_extend = { "sources.default" },
+	"saghen/blink.cmp",
+	version = "1.*",
+	opts = {
+		appearance = {
+			nerd_font_variant = "mono",
+		},
+		sources = {
+			default = { "lsp", "path", "buffer" },
+		},
+		signature = { enabled = true },
+	},
+	opts_extend = { "sources.default" },
 }
