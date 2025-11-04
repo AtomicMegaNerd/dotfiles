42e79473ed3d3c0451c63f4116a5c43c79cc7bd2
Chris Dunphy <chris@megaparsec.ca>
2025-11-04 16:24:20 -0700

    Add ligatures for ghostty

--- nix/ghostty.nix
+++ nix/ghostty.nix
@@ -6,6 +6,8 @@
   settings = {
     font-family = "Monaspace Argon";
     font-size = 14;
+    font-thicken = true;
+    font-feature = "ss01,ss02,ss03,ss04,ss05,ss06,ss07,ss08,ss09,ss10";
     macos-option-as-alt = true;
   };
 }
