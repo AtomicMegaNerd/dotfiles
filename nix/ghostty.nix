cac41ee6a4fc634ca07abebef7d76c1b1ccec3b6
Chris Dunphy <chris@megaparsec.ca>
2025-11-06 15:29:11 -0700

    Update flake

--- nix/ghostty.nix
+++ nix/ghostty.nix
@@ -5,7 +5,7 @@
 
   settings = {
     font-family = "Monaspace Argon";
-    font-size = 16;
+    font-size = 14;
     font-thicken = true;
     font-feature = "ss01,ss02,ss03,ss04,ss05,ss06,ss07,ss08,ss09,ss10,calt,liga";
     macos-option-as-alt = true;
