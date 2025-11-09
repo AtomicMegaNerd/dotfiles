47ccbf187ddf118ea6eaf8f2ac46e01da8159076
Chris Dunphy <chris@megaparsec.ca>
2025-11-09 09:31:34 -0700

    Install Zed remote server

--- hosts/blahaj/rcd.nix
+++ hosts/blahaj/rcd.nix
@@ -7,7 +7,10 @@
     packages = import ../../nix/packages.nix { inherit pkgs; };
   };
 
-  programs = import ../../nix/hm_common.nix { inherit pkgs; };
+  programs = import ../../nix/hm_common.nix { inherit pkgs; } // {
+    # This allows Zed to use its remote server feature to connect to this machine
+    zed-editor.installRemoteServer = true;
+  };
 
   catppuccin = import ../../nix/catppuccin.nix;
   xdg.configFile = import ../../nix/xdg.nix;
