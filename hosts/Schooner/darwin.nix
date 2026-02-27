1e9f0408b23e12db50e7e9871c5ff00bb7925f6c
Chris Dunphy <chris@megaparsec.ca>
2026-02-27 12:18:08 -0700

    Fix ghostty

--- hosts/Schooner/darwin.nix
+++ hosts/Schooner/darwin.nix
@@ -3,7 +3,28 @@
   networking.hostName = "Schooner";
   system.stateVersion = 4;
   system.primaryUser = "rcd";
-  nix.enable = false;
+
+  nix.settings = {
+    experimental-features = [
+      "nix-command"
+      "flakes"
+    ];
+    max-jobs = "auto";
+    trusted-users = [ "@admin" ];
+  };
+
+  nix.gc = {
+    automatic = true;
+    interval = {
+      Weekday = 0;
+      Hour = 2;
+      Minute = 0;
+    };
+    options = "--delete-older-than 30d";
+  };
+
+  nix.optimise.automatic = true;
+  ids.gids.nixbld = 350;
 
   programs.fish.enable = true;
 
