0c2d9f76d96a19fc845bdbb8719221c101843903
Chris Dunphy <chris@megaparsec.ca>
2026-02-26 22:31:23 -0700

    Fix lua formatting

--- config/nvim/lua/amn/plugins/conform.lua
+++ config/nvim/lua/amn/plugins/conform.lua
@@ -1,24 +1,25 @@
 return {
-	"stevearc/conform.nvim",
-	opts = {},
-	config = function()
-		local utils = require("amn.utils")
-		local conform = utils.do_import("conform")
-		if not conform then
-			return
-		end
-		conform.setup({
-			formatters_by_ft = {
-				javascript = { "prettier" },
-				json = { "prettier" },
-				yaml = { "prettier" },
-				html = { "prettier" },
-				css = { "prettier" },
-			},
-			format_on_save = {
-				timeout_ms = 500,
-				lsp_fallback = true,
-			},
-		})
-	end,
+  "stevearc/conform.nvim",
+  opts = {},
+  config = function()
+    local utils = require("amn.utils")
+    local conform = utils.do_import("conform")
+    if not conform then
+      return
+    end
+    conform.setup({
+      formatters_by_ft = {
+        javascript = { "prettier" },
+        json = { "prettier" },
+        yaml = { "prettier" },
+        html = { "prettier" },
+        css = { "prettier" },
+        lua = { "stylua" },
+      },
+      format_on_save = {
+        timeout_ms = 500,
+        lsp_fallback = true,
+      },
+    })
+  end,
 }
--- config/nvim/lua/amn/plugins/oil.lua
+++ config/nvim/lua/amn/plugins/oil.lua
@@ -1,23 +1,22 @@
 return {
-	"stevearc/oil.nvim",
-	config = function()
-		local utils = require("amn.utils")
-		local oil = utils.do_import("oil")
-		if not oil then
-			return
-		end
+  "stevearc/oil.nvim",
+  config = function()
+    local utils = require("amn.utils")
+    local oil = utils.do_import("oil")
+    if not oil then
+      return
+    end
 
-		oil.setup({
-			columns = {
-				"icon",
-				"permissions",
-				"size",
-				"mtime",
-			},
-			view_options = {
-				show_hidden = true,
-			},
-		})
-		utils.nmap("<leader>o", "<Cmd>Oil<CR>", "Open current directory in Oil")
-	end,
+    oil.setup({
+      columns = {
+        "icon",
+        "size",
+        "mtime",
+      },
+      view_options = {
+        show_hidden = true,
+      },
+    })
+    utils.nmap("<leader>o", "<Cmd>Oil<CR>", "Open current directory in Oil")
+  end,
 }
