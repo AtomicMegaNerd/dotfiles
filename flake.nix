9ec88c4d85f26d4880113b0d4ed75344a3460352
Chris Dunphy <chris@megaparsec.ca>
2026-03-07 19:40:40 -0700

    Fix input

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "AtomicMegaNerd's NixOS Flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/release-25.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
