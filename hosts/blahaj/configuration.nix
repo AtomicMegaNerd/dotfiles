fbbc077b3f1abe11c79ec384f58617e54eae5de7
Chris Dunphy <chris@megaparsec.ca>
2026-03-18 18:53:37 -0600

    Update freshrss

--- hosts/blahaj/configuration.nix
+++ hosts/blahaj/configuration.nix
@@ -95,7 +95,7 @@ in
 
       freshrss = {
         autoStart = true;
-        image = "freshrss/freshrss:1.28.0";
+        image = "freshrss/freshrss:1.28.1";
         ports = [
           "8080:80/tcp"
         ];
