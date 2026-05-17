6f68091a66d65b875863133bb03c30322b79560f
Chris Dunphy <chris@megaparsec.ca>
2026-05-17 12:38:51 -0600

    Only use ed25519 keys

--- hosts/blahaj/configuration.nix
+++ hosts/blahaj/configuration.nix
@@ -145,6 +145,12 @@ in
 
   services.openssh = {
     enable = true;
+    hostKeys = [
+      {
+        path = "/etc/ssh/ssh_host_ed25519_key";
+        type = "ed25519";
+      }
+    ];
     settings = {
       PasswordAuthentication = false;
       PermitRootLogin = "no";
