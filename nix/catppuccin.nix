6fc56dbd224b68889140575affe4d55161f4373f
Chris Dunphy <chris@megaparsec.ca>
2026-05-31 14:04:03 -0600

    Update nix flake

--- nix/catppuccin.nix
+++ nix/catppuccin.nix
@@ -1,8 +1,8 @@
 {
   enable = true;
+  autoEnable = true;
   flavor = "macchiato";
   accent = "sky";
-  btop.enable = true;
   # We have our own neovim config with catppuccin
   nvim.enable = false;
 }
