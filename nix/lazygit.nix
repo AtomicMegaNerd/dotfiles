6dfb30b6b6b6d6259d4e85be5a6702e6844309d7
Chris Dunphy <chris@megaparsec.ca>
2026-06-01 19:08:14 -0600

    Add lazygit config

--- nix/lazygit.nix
+++ nix/lazygit.nix
@@ -1,3 +1,20 @@
 {
   enable = true;
+  settings = {
+    gui = {
+      mouseEvents = false;
+      timeFormat = "2006-01-02";
+      shortTimeFormat = "15:04";
+      showRandomTip = false;
+      showBottomLine = false;
+      nerdFontsVersion = "3";
+      commandLogSize = 12;
+    };
+    git = {
+      autoFetch = false;
+      autoRefresh = false;
+    };
+    os.editPreset = "nvim";
+    update.method = "never";
+  };
 }
