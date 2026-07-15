a4a8ae8fe13199d71cd8ecb133a32809620d6e00
Chris Dunphy <chris@megaparsec.ca>
2026-07-15 14:35:30 -0600

    Update docs

--- hosts/blahaj/starfeed.nix
+++ hosts/blahaj/starfeed.nix
@@ -1,9 +1,19 @@
 { config, ... }:
 {
+  # This user maps to the UID of the default noroot user in the distroless images like
+  # gcr.io/distroless/static-debian13:nonroot. We need to give this user permissions to
+  # the decrypted starfeed.toml file
   users.users.starfeed = {
     isSystemUser = true;
     uid = 65532;
     group = "nogroup";
+  };
+
+  # We give ownership of the decrypted file to this starfeed user
+  age.secrets.starfeed-config = {
+    file = ../../secrets/starfeed-config.age;
+    owner = "starfeed";
+    mode = "0400";
   };
 
   virtualisation.oci-containers.containers.starfeed = {
@@ -24,9 +34,4 @@
     before = [ "podman-starfeed.service" ];
   };
 
-  age.secrets.starfeed-config = {
-    file = ../../secrets/starfeed-config.age;
-    owner = "starfeed";
-    mode = "0400";
-  };
 }
