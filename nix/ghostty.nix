13d42eb78fc83da0f95c4127a78127e94cdc7b17
Chris Dunphy <chris@megaparsec.ca>
2026-07-19 08:53:42 -0600

    Fixes to ghostty

--- nix/ghostty.nix
+++ nix/ghostty.nix
@@ -7,11 +7,12 @@
     settings = {
       font-family = "Monaspace Argon";
       font-size = 16;
+      font-feature = "ss01,ss02,ss03,ss04,ss05,ss06,ss07,ss08,ss09,ss10,calt,liga";
       theme = "catppuccin-frappe";
       cursor-style = "block";
       cursor-style-blink = true;
       macos-option-as-alt = true;
-      shell-integration-features = "no-cursor,ssh-env,ssh-terminfo,sudo";
+      shell-integration-features = "no-cursor,ssh-env,ssh-terminfo,sudo,no-title";
     };
   };
 }
