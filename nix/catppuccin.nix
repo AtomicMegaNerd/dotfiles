a91758eadef999f793d19c109e2d0d4702b9b013
Chris Dunphy <chris@megaparsec.ca>
2026-07-25 18:45:32 -0600

    fix catppuccin module

--- nix/catppuccin.nix
+++ nix/catppuccin.nix
@@ -1,10 +1,10 @@
-{ ... }:
+{ config, lib, ... }:
 {
   catppuccin = {
-    autoEnable = true;
-    enable = true;
-    flavor = "frappe";
-    accent = "sapphire";
+    enable = config.amnOptions.theme == "catppuccin";
+    autoEnable = lib.mkIf (config.amnOptions.theme == "catppuccin") true;
+    flavor = lib.mkIf (config.amnOptions.theme == "catppuccin") "frappe";
+    accent = lib.mkIf (config.amnOptions.theme == "catppuccin") "sapphire";
     # Neovim is configured separately, see github.com/atomicmeganerd/rcd-nvim
     nvim.enable = false;
   };
