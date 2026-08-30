b276a13aed0489d83b21f326f7a1842e8201888d
Chris Dunphy <chris@megaparsec.ca>
2026-08-29 21:06:10 -0600

    zellij fixes

--- nix/zellij.nix
+++ nix/zellij.nix
@@ -13,11 +13,10 @@ in
     enableFishIntegration = false;
 
     settings = {
-      # simplified_ui = true;
+      simplified_ui = true;
       default_mode = "locked";
       default_layout = "compact_with_nvim";
       session_serialization = false;
-      pane_frames = false;
 
       # Stylix generates a theme named "default" in themes/stylix.kdl
       theme = lib.mkIf (config.amnOptions.theme == "stylix") "default";
